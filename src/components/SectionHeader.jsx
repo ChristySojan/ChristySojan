f82358979e4553e441e40c8d41dd82bb2c40384b
Christy Sojan <sojanchristy4@gmail.com>
2025-09-24 20:53:08 +0530

    Delete src directory

--- src/components/SectionHeader.jsx
+++
@@ -1,8 +0,0 @@
-export default function SectionHeader({ title, subtitle }) {
-  return (
-    <div className="mb-8">
-      <h1 className="h1">{title}</h1>
-      {subtitle && <p className="mt-2 muted">{subtitle}</p>}
-    </div>
-  )
-}
